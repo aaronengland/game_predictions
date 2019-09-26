6aa36c6d746247904f8650237eecc62f93dd8574
Aaron England <aaron.england24@gmail.com>
2019-09-25 23:01:11 -0700

    Update __init__.py

--- game_predictions/__init__.py
+++ game_predictions/__init__.py
@@ -15,62 +15,66 @@ def game_predictions(home_team_array, home_score_array, away_team_array, away_sc
     away_score_prediction_list = []
     # iterate through n_simulations
     for i in range(n_simulations):
-        # 1. get mean points scored by the homne team when they are the home team
-        # subset to games where home_team == home_team
-        df_home = df[df['home_team'] == home_team]
-        # create a list of game number so we can use them as weights
-        list_weights = list(np.arange(1,df_home.shape[0]+1))
-        # include logic for weighted mean
-        if weighted_mean == True:
-            # get weighted mean of home_score
-            home_score_mean = np.average(df_home['home_score'], weights=list_weights)
-        else:
-            # get mean of home_score
-            home_score_mean = np.mean(df_home['home_score'])
-        # generate a random number from a poisson distribution with this number as the lambda
-        pred_home_score = np.random.poisson(home_score_mean, 1)
-        # 2. get mean points allowed by the home team when they are the home team
-        # include logic for weighted mean
-        if weighted_mean == True:
-            # get weighted mean of away_score
-            pred_home_opponent_mean = np.average(df_home['away_score'], weights=list_weights)
-        else:
-            # get mean of away_score
-            pred_home_opponent_mean = np.mean(df_home['away_score'])
-        # generate a random number from a poisson distribution with this number as the lambda
-        pred_home_opponent_score = np.random.poisson(pred_home_opponent_mean, 1)
-        
-        # 3. get mean points scored by the away team when they are away
-        # subset to games where away_team == away_team
-        df_away = df[df['away_team'] == away_team]
-        # create a list of game number so we can use them as weights
-        list_weights = list(np.arange(1,df_away.shape[0]+1))
-        # include logic for weighted mean
-        if weighted_mean == True:
-            # get weighted mean of away_score
-            away_score_mean = np.average(df_away['away_score'], weights=list_weights)
-        else:
-            # get mean of away_score
-            away_score_mean = np.mean(df_away['away_score'])
-        # generate a random number from a poisson distribution with this number as the lambda
-        pred_away_score = np.random.poisson(away_score_mean, 1)
-        # 4. get mean points allowed by the away team when they are away
-        # include logic for weighted mean
-        if weighted_mean == True:
-            # get weighted mean of home_score
-            pred_away_opponent_mean = np.average(df_away['home_score'], weights=list_weights)
-        else:
-            # get mean of home_score
-            pred_away_opponent_mean = np.mean(df_away['home_score'])
-        # generate a random number from poisson distribution with this number as the lambda
-        pred_away_opponent_score = np.random.poisson(pred_away_opponent_mean, 1)
-            
-        # now let's have the scores meet in the middle
-        # for the home team, get the average of pred_home_score and pred_away_opponent_score
-        home_score_prediction = (pred_home_score[0] + pred_away_opponent_score[0])/2
-        # for the away team, get the average of the pred_away_score and 
-        away_score_prediction = (pred_away_score[0] + pred_home_opponent_score[0])/2
-            
+        # set equal
+        home_score_prediction = 0
+        away_score_prediction = 0
+        # to make sure there arent ties
+        while home_score_prediction == away_score_prediction:
+            # 1. get mean points scored by the homne team when they are the home team
+            # subset to games where home_team == home_team
+            df_home = df[df['home_team'] == home_team]
+            # create a list of game number so we can use them as weights
+            list_weights = list(np.arange(1,df_home.shape[0]+1))
+            # include logic for weighted mean
+            if weighted_mean == True:
+                # get weighted mean of home_score
+                home_score_mean = np.average(df_home['home_score'], weights=list_weights)
+            else:
+                # get mean of home_score
+                home_score_mean = np.mean(df_home['home_score'])
+            # generate a random number from a poisson distribution with this number as the lambda
+            pred_home_score = np.random.poisson(home_score_mean, 1)
+            # 2. get mean points allowed by the home team when they are the home team
+            # include logic for weighted mean
+            if weighted_mean == True:
+                # get weighted mean of away_score
+                pred_home_opponent_mean = np.average(df_home['away_score'], weights=list_weights)
+            else:
+                # get mean of away_score
+                pred_home_opponent_mean = np.mean(df_home['away_score'])
+            # generate a random number from a poisson distribution with this number as the lambda
+            pred_home_opponent_score = np.random.poisson(pred_home_opponent_mean, 1)
+
+            # 3. get mean points scored by the away team when they are away
+            # subset to games where away_team == away_team
+            df_away = df[df['away_team'] == away_team]
+            # create a list of game number so we can use them as weights
+            list_weights = list(np.arange(1,df_away.shape[0]+1))
+            # include logic for weighted mean
+            if weighted_mean == True:
+                # get weighted mean of away_score
+                away_score_mean = np.average(df_away['away_score'], weights=list_weights)
+            else:
+                # get mean of away_score
+                away_score_mean = np.mean(df_away['away_score'])
+            # generate a random number from a poisson distribution with this number as the lambda
+            pred_away_score = np.random.poisson(away_score_mean, 1)
+            # 4. get mean points allowed by the away team when they are away
+            # include logic for weighted mean
+            if weighted_mean == True:
+                # get weighted mean of home_score
+                pred_away_opponent_mean = np.average(df_away['home_score'], weights=list_weights)
+            else:
+                # get mean of home_score
+                pred_away_opponent_mean = np.mean(df_away['home_score'])
+            # generate a random number from poisson distribution with this number as the lambda
+            pred_away_opponent_score = np.random.poisson(pred_away_opponent_mean, 1)
+
+            # now let's have the scores meet in the middle
+            # for the home team, get the average of pred_home_score and pred_away_opponent_score
+            home_score_prediction = (pred_home_score[0] + pred_away_opponent_score[0])/2
+            # for the away team, get the average of the pred_away_score and 
+            away_score_prediction = (pred_away_score[0] + pred_home_opponent_score[0])/2
         # append to lists
         # home
         home_score_prediction_list.append(home_score_prediction)
@@ -80,7 +84,7 @@ def game_predictions(home_team_array, home_score_array, away_team_array, away_sc
     # get the proportion of games where the home team is > away team
     sum_home_wins = 0
     for i in range(len(home_score_prediction_list)):
-        if home_score_prediction_list[i] >= away_score_prediction_list[i]:
+        if home_score_prediction_list[i] > away_score_prediction_list[i]:
             sum_home_wins += 1
     prop_home_win = sum_home_wins/len(home_score_prediction_list)
 
